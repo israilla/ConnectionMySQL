53e52f3ed6447848ac6005031bdff37e243c483f
Israilla <israillacaroline@hotmail.com>
2020-02-25 22:20:09 -0300

    Conectando C# ao MySQL utilizando ASP.NET (Aplicativo Web)

---
+++ ConnectionMySQL/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
+
+namespace ConnectionMySQL
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection("server=localhost; User Id=root; Database=COMERCIO; Password=caroli@#db1597");
+            MySqlCommand comando = new MySqlCommand ("SELECT * FROM CLIENTE", conexao);
+            DataTable Cliente = new DataTable();
+            try
+            {
+                conexao.Open(); 
+                gdvDados.DataSource = comando.ExecuteReader();
+                gdvDados.DataBind();
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
